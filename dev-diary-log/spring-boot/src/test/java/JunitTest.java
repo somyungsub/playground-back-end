fe63fe5eec0123a0a9a5694edfe24c25d3b6a53b
myungsubso <myungsubso@hanwha.com>
2025-09-18 13:35:06 +0900

    dev-diary-log 모듈 수정

--- dev-diary-log/spring-boot/src/test/java/JunitTest.java
+++ dev-diary-log/spring-boot/src/test/java/JunitTest.java
@@ -1,26 +1,26 @@
-import org.junit.jupiter.api.Test;
-import org.mockito.MockedStatic;
-import org.mockito.Mockito;
+//import org.junit.jupiter.api.Test;
+//import org.mockito.MockedStatic;
+//import org.mockito.Mockito;
 
 public class JunitTest {
 
-  @Test
-  void test() {
-    System.out.println("test");
-    StaticTest.print();
-  }
-
-  @Test
-  void test2() {
-    try (MockedStatic<StaticTest> staticTestMockedStatic = Mockito.mockStatic(StaticTest.class)) {
-      StaticTest.print();
-      staticTestMockedStatic.verify(StaticTest::print);
-    }
-  }
-
-  static class StaticTest {
-    static void print() {
-      System.out.println("static");
-    }
-  }
+//  @Test
+//  void test() {
+//    System.out.println("test");
+//    StaticTest.print();
+//  }
+//
+//  @Test
+//  void test2() {
+//    try (MockedStatic<StaticTest> staticTestMockedStatic = Mockito.mockStatic(StaticTest.class)) {
+//      StaticTest.print();
+//      staticTestMockedStatic.verify(StaticTest::print);
+//    }
+//  }
+//
+//  static class StaticTest {
+//    static void print() {
+//      System.out.println("static");
+//    }
+//  }
 }
